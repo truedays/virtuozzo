81e7fac5c9da5e733889248c4b95d3c41ef1a938
Ray <ray@truedays.org>
2013-09-03 12:47:24 -0500

    minor patch

--- highload.sh
+++ highload.sh
@@ -46,7 +46,7 @@ echo "$1 $cur_epoc $cur_time" >> $lockfile
 echo $cur_date > $tmpfile
 vzctl exec $1 '(echo -e "$HOSTNAME $(cat /proc/vz/veinfo_redir)\n______\n";export COLUMNS=200;/usr/bin/top -bcMn 1|head -n50;echo "+++end+++";echo)' >> $tmpfile
 cat $tmpfile | mail -s "HIGHLOAD: vps${1} restarted on $HOSTNAME" $mailto
-echo -n "$cur_date CTID: $1 LOAD: $(vzlist -Ho laverage $1) | tee -a $logfile
+echo -n "$cur_date CTID: $1 LOAD: $(vzlist -Ho laverage $1)" | tee -a $logfile
 
 ####  Main forloop to check for high load
 # vzlist without header (-H) with columns ctid and load average. tr to simplify parsing. awk script to print CTIDs when load > max allowed.
