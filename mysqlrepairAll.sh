705d53df9cf00e5b44958e9bab75d3df11a5ead4
truedays <ray@truedays.org>
2014-01-29 18:30:49 -0600

    echos

--- mysqlrepairAll.sh
+++ mysqlrepairAll.sh
@@ -15,6 +15,8 @@ for each in `vzlist -Ho ctid,diskspace -s diskspace | grep " 0$"| awk '{print $1
 done
 
 # echo dots while these $PIDS still exist (not well tested)
+echo "Tracking PIDs: $PIDS"
+echo
 while /bin/ps $PIDS >/dev/null 2>&1
  do 
  echo -n ".";
