971358bfe5a35bea7ca5f858a824ec4845db418e
truedays <ray@truedays.org>
2014-01-29 18:10:54 -0600

    mysqlrepairAll

---
+++ mysqlrepairAll.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+#
+# repair all containers mysql tables
+#
+
+# mysqlcheck -uroot --auto-repair --check --all-databases | grep -v OK
+
+
+for each in `vzlist -Ho ctid,diskspace -s diskspace | grep " 0$"| awk '{print $1}'`
+ do
+ echo "+++ Requesting mysql repair on $each.."
+ /usr/sbin/vzctl exec $each 'mysqlcheck -uroot --auto-repair --check --all-databases | grep -v OK'&
+ PIDS+="$! "
+ echo
+done
+
+# echo dots while these $PIDS still exist (not well tested)
+while /usr/bin/ps $PIDS
+ do 
+ echo -n ".";
+ sleep 1
+done
+
