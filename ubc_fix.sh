011682eee49ff89271cdfc7479ce37783f57215b
truedays <ray@truedays.org>
2014-03-21 13:59:32 -0500

    +backup

--- ubc_fix.sh
+++ ubc_fix.sh
@@ -3,10 +3,15 @@
 # find old 256M and 512M containers and upgrade them to 1G UBC
 #
 
+[ -d /root/ubc_fix_backups ] || mkdir /root/ubc_fix_backups
+
+BACKUPDIR="/root/ubc_fix_backups"
 
 for each in $(grep -lE 'PHYSPAGES="131072:131072"|PHYSPAGES="65536:65536"' /etc/vz/conf/*.conf | sed 's/.*\///' | cut -d. -f1 )
  do
  echo $each
+ [ -f ${BACKUPDIR}/${each}.conf ] || ( echo "Backing up ${each}.."; cp -v /etc/vz/conf/${each}.conf ${BACKUPDIR}/ )
+
 done
 
 
