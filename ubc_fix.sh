d65cefb4142f297249ac1ea7c84c1bf042fafaa5
truedays <ray@truedays.org>
2014-03-21 14:02:12 -0500

    final touches

--- ubc_fix.sh
+++ ubc_fix.sh
@@ -10,7 +10,11 @@ BACKUPDIR="/root/ubc_fix_backups"
 for each in $(grep -lE 'PHYSPAGES="131072:131072"|PHYSPAGES="65536:65536"' /etc/vz/conf/*.conf | sed 's/.*\///' | cut -d. -f1 )
  do
  echo $each
+ # Backup CONFs
  [ -f ${BACKUPDIR}/${each}.conf ] || ( echo "Backing up ${each}.."; cp -v /etc/vz/conf/${each}.conf ${BACKUPDIR}/ )
+
+ # Apply changes
+ vzctl set $each --physpages :262144 --lockedpages 262144:262144 --privvmpages 262144:262144 --vmguarpages 262144:262144 --oomguarpages 262144:262144 --save
 
 done
 
